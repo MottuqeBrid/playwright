3d6592523a4b4c8388d258aeeb1e936a844f0c62
MottuqeBrid <mottuqebrid@gmail.com>
2026-08-02 00:00:55 +0600

    login and logout

--- 01 basics/01 bsiec.py
+++ 01 basics/01 bsiec.py
@@ -1,19 +1,36 @@
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
+url = "https://www.saucedemo.com/"
+password = "secret_sauce"
+user_name = "standard_user"
+
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=False, slow_mo=1000)
     context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
-    page.goto("https://www.google.com/?zx=1785599076480")
-    page.get_by_role("combobox", name="Search").click()
-    page.get_by_role("combobox", name="Search").fill("playwright")
+    page.goto(url)
+
+    page.locator("#user-name").fill(user_name,timeout=2000)
+    page.locator("#password").fill(password,timeout=2000)
+    page.locator("#login-button").click()
+
+    product_header = page.locator("//span[text()='Products']")
+    assert product_header.is_visible(), "User is unable to login"
+
+    burger_menu = page.locator("#react-burger-menu-btn")
+    burger_menu.click()
+    logout_btn = page.locator("#logout_sidebar_link")
+    assert logout_btn.is_visible(), "Logout button is not visible"
+    logout_btn.click()
+
+    login_btn = page.locator("#login-button")
+    assert login_btn.is_visible(), "Login button is not visible"
+
 
     # ---------------------
     # Stop tracing and export it into a zip archive.
-    context.tracing.stop(path="trace.zip")
     context.close()
     browser.close()
 
